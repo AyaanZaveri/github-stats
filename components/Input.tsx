c725e4a94f6a02dda1d047afd2eb55971f7327a1
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-10 12:13:08 -0500

    Changing theme to orange (git colors).

--- components/Input.tsx
+++ components/Input.tsx
@@ -14,7 +14,7 @@ const Input = ({ repo, setRepo, user, setUser, handleSubmit }: Props) => {
       <form onSubmit={handleSubmit}>
         <input
           placeholder="Profile"
-          className="focus:ring-blue w-72 text-gray-500 mt-3 shadow-sm appearance-none inline-flex items-center px-2 py-2 hover:text-gray-500 focus:text-gray-500 hover:bg-slate-50 active:text-gray-500 text-sm font-normal active:bg-blue-100 bg-white border focus:border-blue-600 focus:ring ring-blue-300 rounded-md focus:outline-none transition"
+          className="focus:ring-orange w-72 text-gray-500 mt-3 shadow-sm appearance-none inline-flex items-center px-2 py-2 hover:text-gray-500 focus:text-gray-500 hover:bg-slate-50 active:text-gray-500 text-sm font-normal active:bg-orange-100 bg-white border focus:border-orange-600 focus:ring ring-orange-300 rounded-md focus:outline-none transition"
           value={user}
           onChange={(e) => setUser(e.target.value)}
         />
@@ -22,7 +22,7 @@ const Input = ({ repo, setRepo, user, setUser, handleSubmit }: Props) => {
       <form onSubmit={handleSubmit}>
         <input
           placeholder="Repository"
-          className="focus:ring-blue w-72 text-gray-500 mt-3 shadow-sm appearance-none inline-flex items-center px-2 py-2 hover:text-gray-500 focus:text-gray-500 hover:bg-slate-50 active:text-gray-500 text-sm font-normal active:bg-blue-100 bg-white border focus:border-blue-600 focus:ring ring-blue-300 rounded-md focus:outline-none transition"
+          className="focus:ring-orange w-72 text-gray-500 mt-3 shadow-sm appearance-none inline-flex items-center px-2 py-2 hover:text-gray-500 focus:text-gray-500 hover:bg-slate-50 active:text-gray-500 text-sm font-normal active:bg-orange-100 bg-white border focus:border-orange-600 focus:ring ring-orange-300 rounded-md focus:outline-none transition"
           value={repo}
           onChange={(e) => setRepo(e.target.value)}
         />
